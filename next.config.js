cbd9f023efa704bd3f70f10c7e301ecf2ed0fda1
newage1911 <newaddsteam2555@gmail.com>
2023-09-03 12:54:38 +0700

    edit: nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["links.papareact.com", "images.unsplash.com", "dummyimage.com"],
